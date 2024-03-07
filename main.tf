93ae98177bf1c77d420d1fecba9123317231d0df
Serhii <37027381+EinErste@users.noreply.github.com>
2024-03-08 00:46:51 +0200

    init

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "SerhiiSLab5"
+  vpc_security_group_ids = ["sg-01142ec3c87ec19dd"]
+  tags = {
+    Name = "SerhiiSLab5Terraform"
+  }
+}
